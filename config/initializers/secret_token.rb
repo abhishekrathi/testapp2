adb6f0a8f9809d44468f71e7c2fb68d4b45a40a9
Abhishek Rathi <rathi.abhishek@gmail.com>
2013-02-02 21:32:26 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidclips::Application.config.secret_token = '57d41e770c7daa7393d822784fc2bd76a74950ca7607c9ee5c8b68bd6665028f2419f999921536d3ed4904ef36e1573992042987156680d226c640c9bd257945'
